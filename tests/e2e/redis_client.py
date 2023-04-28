bdc2c94d38175370b83aae3b69e7080212767c2d
Allan Simbajon <asimbajon1@buffs.wtamu.edu>
2023-04-28 07:51:19 -0700

    Ready for grading

--- tests/e2e/redis_client.py
+++ tests/e2e/redis_client.py
@@ -1,18 +1,18 @@
-import json
-import redis
+# import json
+# import redis
 
-from allocation import config
+# from allocation import config
 
-r = redis.Redis(**config.get_redis_host_and_port())
-
-
-def subscribe_to(channel):
-    pubsub = r.pubsub()
-    pubsub.subscribe(channel)
-    confirmation = pubsub.get_message(timeout=3)
-    assert confirmation["type"] == "subscribe"
-    return pubsub
+# r = redis.Redis(**config.get_redis_host_and_port())
 
 
-def publish_message(channel, message):
-    r.publish(channel, json.dumps(message))
+# def subscribe_to(channel):
+#     pubsub = r.pubsub()
+#     pubsub.subscribe(channel)
+#     confirmation = pubsub.get_message(timeout=3)
+#     assert confirmation["type"] == "subscribe"
+#     return pubsub
+
+
+# def publish_message(channel, message):
+#     r.publish(channel, json.dumps(message))
